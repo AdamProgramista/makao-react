5505bc546efd9829f100f70d23e1c009e0d1a678
AdamProgramista <adam.programista@poczta.fm>
2018-04-28 10:51:08 +0200

    front/login-page 1.4

--- front/src/modules/Game/GameInfo.js
+++ front/src/modules/Game/GameInfo.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
 import './Game.css';
 
-
+const players = [{name: 'Monika'}, {name: 'Adam'}, {name: 'Mirek'}, {name: 'Yuriy'}]
 
 class GameInfo extends Component {
-  // players = [{name: Monika}, {name: Adam}, {name: Mirek}, {name: Yuriy}]
-
   render() {
     return (
       <div className='game__info'>
         <div className='game__info__header'>
           <h3>Makao-react</h3>
         </div>
-        <div className='game__info__list'>
-          {/* <ul>
-          {this.players.map(p => (
-            <li>p.name</li>
+        <ul className='game__info__list'>
+          {players.map(p => (
+            <li>{p.name}</li>
           ))}
-          </ul> */}
-        </div>
+        </ul>
       </div>
     );
   }
